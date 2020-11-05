7b6623c88b928417984e2d0c279480668eff157a
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-11-05 13:37:27 -0600

    Get city name, current date, temperature, humidity, and wind speed. Still need to get the UV index from different url call

--- weather.js
+++ weather.js
@@ -8,11 +8,18 @@ $(document).ready(function() {
         let myAPIkey = "0906529f0e63b6d51e5251dfa653e405";
 
         // Get the search city input from the user
-        let city = $("#city-search").val();
-        console.log(city);
+        let cityName = $("#city-search").val();
+
+        let cityList = $("#city-list");
+        let city = $("<a>");
+        city.addClass("list-group-item list-group-item-action city");
+        city.text(cityName);
+
+        cityList.append(city);
+
 
         // URL where the data is from
-        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myAPIkey;
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + myAPIkey;
 
         // Create an AJAX call to retrieve the data from OpenWeather API by the city
         $.ajax({
@@ -21,14 +28,37 @@ $(document).ready(function() {
         }).then(function(response) {
             console.log(response);
 
-            let cityDescription = $(".city-weather");
-            let cityTitleEl = $("<h2>");
-            cityTitleEl.text(response.name);
-
-            cityDescription.append(cityTitleEl);
+            getCityWeatherDescription(response);
         });
     });
 });
+
+function getCityWeatherDescription(city) {
+
+    //$("#city-weather").empty();
+
+    // Get current date
+    let currentDate = moment().format("L");
+
+    // Create elements and add the city weather descriptions to the box
+    let cityTitle = $("<h1>");
+    cityTitle.addClass("city-title");
+    cityTitle.text(city.name + " " + currentDate);
+
+    let cityTemp = $("<p>");
+    cityTemp.addClass("city-temp");
+    cityTemp.text("Temperature: " + city.main.temp);
+
+    let cityHumidity = $("<p>");
+    cityHumidity.addClass("city-humidity");
+    cityHumidity.text("Humidity: " + city.main.humidity);
+
+    let cityWindSpeed = $("<p>");
+    cityWindSpeed.addClass("city-wind-speed");
+    cityWindSpeed.text("Wind Speed: " + city.wind.speed);
+
+    $("#city-weather").append(cityTitle, cityTemp, cityHumidity, cityWindSpeed);
+};
 
 
     
